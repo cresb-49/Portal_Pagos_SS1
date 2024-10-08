81bd88eb33eb9c31443af67e2f731558a3bd2787
Benjamin <carlosbpac@gmail.com>
2024-10-07 21:43:22 -0600

    Add dependencies for jsonwebtoken and @types/jsonwebtoken

--- api_pagos/src/middlewares/authMiddleware.ts
+++ api_pagos/src/middlewares/authMiddleware.ts
@@ -1,11 +1,21 @@
 import { Request, Response, NextFunction } from 'express';
+import jwt from 'jsonwebtoken';
 
-export const authMiddleware = (req: Request, res: Response, next: NextFunction) => {
-    const token = req.headers['authorization'];
-    if (token) {
-        // lógica para verificar el token
-        next();
+const secret = process.env.JWT_SECRET || 'defaultsecret';
+
+export const authenticateJWT = (req: Request, res: Response, next: NextFunction) => {
+    const authHeader = req.headers.authorization;
+
+    if (authHeader) {
+        const token = authHeader.split(' ')[1];  // El token se envía como "Bearer TOKEN"
+        jwt.verify(token, secret, (err, user:any) => {
+            if (err) {
+                return res.sendStatus(403);  // Token no válido
+            }
+            req.user = user;  // Agrega la información del usuario decodificada a la solicitud
+            next();  // Continúa con la siguiente función de middleware
+        });
     } else {
-        res.status(403).json({ message: 'Unauthorized' });
+        res.sendStatus(401);  // Token no proporcionado
     }
 };
