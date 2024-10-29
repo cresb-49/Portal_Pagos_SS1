ab20bfaf7c252ce7aa3e6f7a0895e69eb7abaa86
Benjamin <carlosbpac@gmail.com>
2024-10-29 16:03:20 -0600

    Refactor build configuration and environment variables

--- app_pagos/src/environments/environment.prod.ts
+++ app_pagos/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://localhost:3000/api',
+  apiUrl: 'https://api-pagos-krd2.onrender.com/api',
   encryptionKey: 'lshdgflhasgdfsaf564adfgkjAFS2DFDS45'
 };
