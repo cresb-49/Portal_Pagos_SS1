590aed45184d6716a0093a9b7f54e98cf1c953ff
Benjamin <carlosbpac@gmail.com>
2024-10-30 01:29:14 -0600

    Add banking service functions for credit and debit operations with error handling

--- api_pagos/src/config/config.ts
+++ api_pagos/src/config/config.ts
@@ -1,8 +1,8 @@
 export default {
     port: process.env.PORT || 3000,
     databaseUrl: process.env.DATABASE_URL || "mysql://root:root@localhost:3306/pagos_ss1",
-    banco1: process.env.BANCO1 || "http://localhost:3001",
-    banco2: process.env.BANCO2 || "http://localhost:3002",
+    bancopb: process.env.BANCOPB || "http://localhost:3001",
+    bancopc: process.env.BANCOPC || "http://localhost:3002",
     secretKey: process.env.JWT_SECRET || "secretKey",
     ecommerce1: process.env.ECOMMERCE1 || "http://localhost:3003",
     ecommerce2: process.env.ECOMMERCE2 || "http://localhost:3004",
